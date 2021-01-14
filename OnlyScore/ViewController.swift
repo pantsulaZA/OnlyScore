79351da8487b26a7d4f15c8f647434ef43446334
Jannie Theron <>
2021-01-14 21:53:04 +0200

    Initial commit

---
+++ OnlyScore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnlyScore
+//
+//  Created by Jannie Theron on 2021/01/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
