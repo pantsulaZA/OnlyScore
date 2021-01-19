4e5d760ea38b4e517e4e05f1668867bbcb5ce7c5
Jannie Theron <jannie.theron@gmil.com>
2021-01-19 14:55:33 +0200

    donut view using uicircularview SPM package

--- OnlyScore/Report/ViewController.swift
+++ OnlyScore/Report/ViewController.swift
@@ -1,9 +1,10 @@
 import UIKit
+import UICircularProgressRing
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var creditScore: UILabel!
-    @IBOutlet weak var donut: DonutView!
+    @IBOutlet weak var donut: UICircularProgressRing!
     @IBOutlet weak var spinner: UIActivityIndicatorView!
     @IBOutlet weak var error: UILabel!
     @IBOutlet weak var errorView: UIView!
@@ -26,6 +27,7 @@ class ViewController: UIViewController {
             donut.isHidden = false
             spinner.stopAnimating()
             creditScore.text = "\(report.score)"
+            donut.value = CGFloat(report.score)
             errorView.isHidden = true
         case .loading:
             donut.isHidden = true
